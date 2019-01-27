e18d0a7975566cec48c47d993560f7c55636b352
William Meaton <will.meaton@gmail.com>
2019-01-27 11:04:21 +0000

    serve folder instead of each file

--- main.go
+++ main.go
@@ -138,7 +138,7 @@ func main() {
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, data_path+"websockets.html")
+		http.ServeFile(w, r, http.FileServer(http.Dir(data_path+"html")))
 	})
 
 	http.ListenAndServe(":8080", nil)
